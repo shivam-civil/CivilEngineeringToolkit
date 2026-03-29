fc92298d5ce1023dc844076f9f56e72821f9d53a
idocollab <ryuzaki.till@gmail.com>
2026-03-29 22:12:52 +0545

    add webapp.py

---
+++ webapp.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+# Title and icon for all pages 
+st.set_page_config(
+    page_title="CivilEngineeringTools",
+    page_icon="🧱"
+)
+
+# Configure Pages 
+
+levellinG=st.Page(
+    page="streamlit_pages/levelling.py",
+    title="Levelling Automation",
+    icon="💠"
+)
+traversE=st.Page(
+    page="streamlit_pages/traverse.py",
+    title="Traverse Automation",
+    icon="💠"
+)
+developeR=st.Page(
+    page="streamlit_pages/developer.py",
+    title="Developer",
+    icon="👨‍💻",
+    default=True
+)
+
+# naviagtion
+nav = st.navigation(
+    {
+        "Tools":[levellinG,traversE],
+        "Info":[developeR]
+    }
+)
+# run the navigation
+nav.run()
+
+# Shared on nav bar on all pages 
+st.sidebar.markdown("Made with 💌 by Shivam")
