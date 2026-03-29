98bcf43052f1f7ac6bf8999a2b15fe922af9e225
idocollab <ryuzaki.till@gmail.com>
2026-03-29 23:18:35 +0545

    add traverse frontend part

--- streamlit_pages/traverse.py
+++ streamlit_pages/traverse.py
@@ -1,2 +1,33 @@
 import streamlit as st 
-st.header("Traverse Page")
+import pandas as pd
+
+with st.form("myform"):
+    # --- HEADER ----------
+    st.title("Traverse Automation Tool")
+    st.write(" ")
+    st.markdown("Note: Enter the ForeBearings in ***decimal format*** not in degree-minute-second format.")
+    st.write(" ")
+
+    # ----INPUT SECTION ---
+    data=pd.DataFrame(
+        {
+            "Station": ["AB","BC"],
+            "Distance(m)":[0.0,0.0],
+            "ForeBearing(WCB)":[0.0,0.0]
+        }
+    )
+    edited_data=st.data_editor(
+        data,
+        num_rows="dynamic",
+        width="stretch",
+        hide_index=True
+    )
+    
+
+
+    # --- SUBMIT SECTION --
+    submitted=st.form_submit_button("Compute Traverse")
+
+if submitted:
+    st.write(edited_data)
+    
